95d606df77376d74b864ac89b69b9fee801dceda
ricardo1gonzalez <https://github.com/ricardo1gonzalez/ProyectoEnsambladores>
2022-12-06 19:29:09 -0600

    Navegacion que no navega

--- main.py
+++ main.py
@@ -1,23 +1,140 @@
-import cv2
+import math
+import random
+import matplotlib.pyplot as plt
 import numpy as np
-cap = cv2.VideoCapture(0)
-redBajo1 = np.array([0, 100, 20], np.uint8)
-redAlto1 = np.array([8, 255, 255], np.uint8)
-redBajo2=np.array([175, 100, 20], np.uint8)
-redAlto2=np.array([179, 255, 255], np.uint8)
-# Comentario para revisar que hace los commits
-while True:
-  ret,frame = cap.read()
-  if ret==True:
-    frameHSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    maskRed1 = cv2.inRange(frameHSV, redBajo1, redAlto1)
-    maskRed2 = cv2.inRange(frameHSV, redBajo2, redAlto2)
-    maskRed = cv2.add(maskRed1, maskRed2)
-    maskRedvis = cv2.bitwise_and(frame, frame, mask= maskRed)        
-    cv2.imshow('frame', frame)
-    cv2.imshow('maskRed', maskRed)
-    cv2.imshow('maskRedvis', maskRedvis)
-    if cv2.waitKey(1) & 0xFF == ord('s'):
-      break
-cap.release()
-cv2.destroyAllWindows()
+import serial
+
+Arduino = serial.Serial("COM7",9600)
+
+# Son los vectores que guargaran las coordenadas
+# Ori es el vector de los origenes(Donde esta el robot en ese momento) para graficar
+# Izq son las posiciones encontradas en la parte izquierda del robot con el sensor de luz y el servo
+# Der son las posiciones encontradas en la parte derecha del robot con el sensor de luz y el servo
+# La forma sera (x,y) o si es necesario (distancia, angulo)
+
+ori = []
+izq = []
+der = []
+
+# Para poder ubicar al robot de forma automatica es necesario guardar los nodos con los cuales podra regresar
+# o ir a donde considere necesario
+
+grafo = {
+  0: [0,0,1]
+}
+
+conexiones = []
+
+# Para poder ubicar a donde voltear, se necesitara un sensor de angulo
+sentido = 1
+posAct = [0,0]
+
+def PosAleatorias(n):
+  x = []
+  y = []
+  aux = 0
+  aux2 = 0
+  aux3 = 0
+
+  for i in range(0, 5000, n):
+    x.append(0)
+    y.append(i)
+    aux = i
+
+  for i in range(0, 8000, n):
+    x.append(i)
+    y.append(aux)
+    aux2 = i
+
+  for i in range(aux, -1, -n):
+    x.append(aux2)
+    y.append(i)
+    aux3 = i
+
+  for i in range(aux2, -1, -n):
+    x.append(i)
+    y.append(aux3)
+
+  return x,y
+
+def LimAlea():
+  a = []
+  b = []
+  for i in range(0,181):
+    print(i)
+    if(i < 65 or i > 115):
+      if(i == 0 or i == 180):
+        r = 500
+        a.append(round(r * math.cos(i),2))
+        b.append(round(r * math.sin(i),2))
+      else:
+        r = round(500 * math.atan(i),2)
+        a.append(round(r * math.cos(i), 2))
+        b.append(round(r * math.sin(i), 2))
+      print(round(r * math.cos(i), 2),',',round(r * math.sin(i), 2))
+  return a,b
+
+
+def Leer():
+  Arduino.write(b'8')
+  cadena = Arduino.read(3)
+  return str(cadena.decode())
+
+def Navega(sentido, nodos, i, pos, conexiones, tam):
+  valor = Leer()
+  if(valor == "0"):
+    Arduino.write(b'3')
+    if sentido == 8:
+      sentido = 1
+    else:
+      sentido +=1
+  else:
+    Arduino.write(b'1')
+    if(sentido == 1):
+      pos[1] += tam
+    elif (sentido == 2):
+      pos[0] += tam*math.cos(45)
+      pos[1] += tam*math.sin(45)
+    elif (sentido == 3):
+      pos[0] += tam
+    elif (sentido == 4):
+      pos[0] += tam * math.cos(45)
+      pos[1] -= tam * math.sin(45)
+    elif (sentido == 5):
+      pos[1] -= tam
+    elif (sentido == 6):
+      pos[0] -= tam * math.cos(45)
+      pos[1] -= tam * math.sin(45)
+    elif (sentido == 7):
+      pos[0] -= tam
+    elif (sentido == 8):
+      pos[0] -= tam * math.cos(45)
+      pos[1] += tam * math.sin(45)
+
+    grafo[i+1] = [pos[0],pos[1],sentido]
+    conexiones.append([i,i+1])
+
+def ejecuta(sentido,nodo, nodo2):
+  a = movimientos[len(movimientos) - 1]
+  if (a == "000"):
+    regresa()
+  elif (a == "001"):
+    Arduino.write('1'.encode('ascii'))
+  elif (a == "010"):
+    Arduino.write('4'.encode('ascii'))
+    Arduino.write('1'.encode('ascii'))
+    if (sentido == 1):
+      sentido = 4
+    else:
+      sentido -= 1
+  elif (a == "100"):
+    Arduino.write('3'.encode('ascii'))
+    Arduino.write('1'.encode('ascii'))
+    if (sentido == 4):
+      sentido = 1
+    else:
+      sentido += 1
+
+print(grafo)
+grafo[1] = [0,1,2]
+print(grafo[1][0])
